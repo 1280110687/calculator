a54c56de3ef9955b34ce83e0c66dead61693c5e6
xiaoming <xmxm@kg.com>
2020-12-27 15:35:13 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #eee;
+}
+</style>
